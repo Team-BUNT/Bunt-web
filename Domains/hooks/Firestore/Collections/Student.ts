09872ee77a154c13744113dd45ea46814fba6412
Blackraven93 <reblackraven@gmail.com>
2022-11-05 21:45:29 +0900

    feat(domain): firestore student class 구현

--- Domains/hooks/Firestore/Collections/Student.ts
+++ Domains/hooks/Firestore/Collections/Student.ts
@@ -1,9 +1,56 @@
 import FirestoreFetcher from "../FirestoreFetcher";
 
-import { Firestore } from "firebase/firestore";
+import {
+  collection,
+  collectionGroup,
+  CollectionReference,
+  DocumentData,
+  Firestore,
+  getDocs,
+  Query,
+  query,
+  QueryConstraint,
+} from "firebase/firestore";
 
 export default class Student extends FirestoreFetcher {
-  constructor(db: Firestore, documentId: string) {
+  condition?: QueryConstraint[];
+  studentRef: CollectionReference<DocumentData>;
+  studentGroupRef: Query<DocumentData>;
+
+  constructor(db: Firestore, documentId: string, condition?: QueryConstraint[]) {
     super(db, documentId);
+    this.condition = condition;
+    this.studentRef = collection(this.db, "students");
+    this.studentGroupRef = collectionGroup(this.db, "students");
+  }
+
+  async fetchData() {
+    if (!this.db) return new Error("Firestore 객체가 없습니다.");
+
+    if (this.documentId === undefined || this.documentId.trim() === "")
+      return new Error("documentId를 인자로 줘야합니다.");
+
+    return this.condition !== undefined && this.condition.length !== 0
+      ? await this.getStudents()
+      : await this.getStudentAll();
+  }
+
+  async getStudentAll() {
+    const querySnapshot = await getDocs(this.studentRef);
+
+    return querySnapshot.docs.map((doc) => ({
+      ...doc.data(),
+    }));
+  }
+
+  async getStudents() {
+    if (this.condition === undefined) return new Error("조건을 입력해야 합니다.");
+
+    const studentsRef = query(this.studentGroupRef, ...this.condition);
+    const querySnapshot = await getDocs(studentsRef);
+
+    return querySnapshot.docs.map((doc) => ({
+      ...doc.data(),
+    }));
   }
 }
--- Domains/hooks/Firestore/FirestoreFetcher.ts
+++ Domains/hooks/Firestore/FirestoreFetcher.ts
@@ -1,12 +1,58 @@
-import { Firestore } from "firebase/firestore";
+import {
+  collection,
+  collectionGroup,
+  CollectionReference,
+  DocumentData,
+  Firestore,
+  getDocs,
+  Query,
+  query,
+  QueryConstraint,
+} from "firebase/firestore";
 
 class FirestoreFetcher {
+  condition?: QueryConstraint[];
+  studioRef: CollectionReference<DocumentData>;
+  studioGroupRef: Query<DocumentData>;
   db: Firestore;
   documentId: string;
 
-  constructor(db: Firestore, documentId: string) {
+  constructor(db: Firestore, documentId: string, condition?: QueryConstraint[]) {
     this.db = db;
     this.documentId = documentId;
+    this.condition = condition;
+    this.studioRef = collection(this.db, "studios");
+    this.studioGroupRef = collectionGroup(this.db, "studios");
+  }
+
+  async fetchData() {
+    if (!this.db) return new Error("Firestore 객체가 없습니다.");
+
+    if (this.documentId === undefined || this.documentId.trim() === "")
+      return new Error("documentId를 인자로 줘야합니다.");
+
+    return this.condition !== undefined && this.condition.length !== 0
+      ? await this.getStudios()
+      : await this.getStudioAll();
+  }
+
+  async getStudioAll() {
+    const querySnapshot = await getDocs(this.studioRef);
+
+    return querySnapshot.docs.map((doc) => ({
+      ...doc.data(),
+    }));
+  }
+
+  async getStudios() {
+    if (this.condition === undefined) return new Error("조건을 입력해야 합니다.");
+
+    const studiosRef = query(this.studioGroupRef, ...this.condition);
+    const querySnapshot = await getDocs(studiosRef);
+
+    return querySnapshot.docs.map((doc) => ({
+      ...doc.data(),
+    }));
   }
 }
 
