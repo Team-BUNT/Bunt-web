8817c2911e62fd59340f044a42feb4546eb18b66
Blackraven93 <reblackraven@gmail.com>
2022-11-07 18:35:09 +0900

    feat(firebase): firestorage에서 이미지 가져오는 기능 구현

--- Domains/firebase.ts
+++ Domains/firebase.ts
@@ -15,4 +15,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const firestore = getFirestore(app);
 const firestorage = getStorage(app);
+
 export { firestore, firestorage };
--- Modules/Registration/Studio/index.tsx
+++ Modules/Registration/Studio/index.tsx
@@ -1,9 +1,11 @@
+/* eslint-disable jsx-a11y/alt-text */
 /* eslint-disable react-hooks/rules-of-hooks */
 import React, { useEffect, useState } from "react";
 import { firestorage } from "../../../Domains/firebase";
 
 import { getDownloadURL, getStorage, ref } from "firebase/storage";
 import styled from "styled-components";
+import Image from "next/image";
 
 const Container = styled.section`
   width: 100%;
@@ -16,33 +18,42 @@ const Container = styled.section`
 `;
 
 const index = () => {
-  const [url, setUrl] = useState("");
+  const [urls, setUrl] = useState<string[]>([]);
 
-  // useEffect(() => {
-  //   const starsRef = ref(firestorage, "/studios/nfstudio.png");
-  //   getDownloadURL(starsRef)
-  //     .then((url) => {
-  //       console.log(url);
-  //       setUrl(url);
-  //     })
-  //     .catch((error) => {
-  //       switch (error.code) {
-  //         case "storage/object-not-found":
-  //           break;
-  //         case "storage/unauthorized":
-  //           break;
-  //         case "storage/canceled":
-  //           break;
-  //         case "storage/unknown":
-  //           break;
-  //       }
-  //     });
-  // }, []);
+  useEffect(() => {
+    const studios = ["nfstudio", "movidic", "bonafide"];
+
+    studios.forEach((studio) => {
+      const starsRef = ref(firestorage, `/studios/${studio}.png`);
+      getDownloadURL(starsRef)
+        .then((url) => {
+          setUrl([...urls, url]);
+        })
+        .catch((error) => {
+          switch (error.code) {
+            case "storage/object-not-found":
+              break;
+            case "storage/unauthorized":
+              break;
+            case "storage/canceled":
+              break;
+            case "storage/unknown":
+              break;
+          }
+        });
+    });
+  }, []);
 
   return (
     <Container>
       <div>클래스 신청하러 가기</div>
-      <div>div</div>
+      <div>
+        {urls.map((url, index) => (
+          <div key={`${url}${index}`}>
+            <Image width={50} height={50} src={url}></Image>
+          </div>
+        ))}
+      </div>
     </Container>
   );
 };
