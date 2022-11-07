9e9bd6c980a7ac78b944c433e295ff313652fc74
Blackraven93 <reblackraven@gmail.com>
2022-11-07 16:53:47 +0900

    add(view): landingPage 제작 완료

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import styles from "../styles/Home.module.css";
+
 import StudioInformation from "./components/StudioInformation";
 import StudioClassCheckList from "./components/StudioClassCheckList";
 import StudentInputForm from "./components/StudentInputForm";
@@ -7,12 +8,14 @@ import firebaseDB from "../Domains/firebase";
 import { timeFormatter } from "../Domains/timeFormatter";
 import { Class, Enrollment } from "../Domains/hooks/Firestore";
 import { orderByTime } from "../Domains/orderByTime";
+import LandingPage from "../Modules/LandingPage/index";
 
 import type { NextPage } from "next";
 import { useEffect, useState } from "react";
 
 import { useForm } from "react-hook-form";
 import { v4 as uuidv4 } from "uuid";
+import styled from "styled-components";
 
 interface IHall {
   name: string;
@@ -33,6 +36,13 @@ interface IClass {
 const Home: NextPage = () => {
   const [enrollment, setEnrollment] = useState<IClass[]>([]);
 
+  const Root = styled.main`
+    display: flex;
+    justify-content: center;
+    width: 100%;
+    height: 100vh;
+  `;
+
   useEffect(() => {
     const classList = new Class(firebaseDB, "classes");
 
@@ -52,11 +62,11 @@ const Home: NextPage = () => {
   const { register, handleSubmit } = useForm();
 
   return (
-    <div className={styles.container}>
+    <Root>
       <HeadMeta title="Bunt"></HeadMeta>
+      <LandingPage></LandingPage>
 
-      <main className={styles.main}>
-        <StudioInformation
+      {/* <StudioInformation
           studioName="Bunt"
           notice={`카드결제시 현장에서 가능합니다.
 사전신청 후 안내 문자로 정확한 가격 안내 해드리고 있습니다. 10/17 ~ 10/23 까지의 수업 사전 신청서입니다.`}
@@ -90,9 +100,8 @@ const Home: NextPage = () => {
 
           <StudentInputForm register={register}></StudentInputForm>
           <button type="submit">신청하기</button>
-        </form>
-      </main>
-    </div>
+        </form> */}
+    </Root>
   );
 };
 
