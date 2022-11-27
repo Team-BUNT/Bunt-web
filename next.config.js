17fcd2151241c626b9afe93707cf01374e4b0b01
Blackraven93 <reblackraven@gmail.com>
2022-11-28 06:39:29 +0900

    hotfix(cors): cors를 위한  config 재수정

--- next.config.js
+++ next.config.js
@@ -6,6 +6,14 @@ const nextConfig = {
     path: "/_next/image",
     domains: ["localhost", "bunt.life", "firebasestorage.googleapis.com"],
   },
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "https://bunt.life/:path*",
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
